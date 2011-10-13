10078e791b4d364cb4931459e65abcc2ffa97131
Timo Rößner <timo.roessner@googlemail.com>
2011-10-13 21:13:14 +0200

    Explicit require gems.

--- lib/ecircle.rb
+++ lib/ecircle.rb
@@ -1,5 +1,11 @@
 require 'bundler'
-Bundler.require :default
+require 'savon'
+
+# TODO Improve requiring of gems.
+# `Bundler.require :default` works for an irb session with `require lib/ecircle` (e.g. Savon exists)
+# but this won't work from within the rails project since `Bundler.require :default` uses the current rails
+# projects Gemfile, not ecircle's one. Not sure if there is a solution for this problem, but I'd
+# rather do requires via bundler instead of explicit requires.
 
 dir = File.dirname(__FILE__)
 
