2bfd1aad1f037ddf4935003a88bfd8bed7b7c044
Timo Rößner <timo.roessner@googlemail.com>
2011-10-28 20:30:42 +0200

    Make send_date flexible.

--- lib/ecircle/helper.rb
+++ lib/ecircle/helper.rb
@@ -3,7 +3,9 @@ module Ecircle
     extend self
 
     def date_format date
-      date.strftime("%Y-%m-%dT%H:%M:%S")
+      tz = date.strftime('%z')
+      matcher = /(.*)(00)/.match(tz) # We need to do that because ecircle won't accept +0200, just +02:00.
+      "#{date.strftime('%Y-%m-%dT%H:%M:%S')}#{matcher[1]}:#{matcher[2]}"
     end
   end
 end
