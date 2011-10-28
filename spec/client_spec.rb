bbc73947377709592c331f443ad9a623268b21e6
Timo Rößner <timo.roessner@googlemail.com>
2011-10-28 20:34:30 +0200

    Fix spec.

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -16,49 +16,51 @@ describe Ecircle::JobPackage do
     }
 
     @doc = <<xml
-<message message-id="new" delete="false">
-  <sendout-preferences>
-    <object-handling html-images="untouched"/>
-    <email-channel preferred-format="email-html-multipart"/>
-  </sendout-preferences>
-  <send-date>
-    <date>#{Ecircle::Helper.date_format(@options[:send_out_date])}</date>
-  </send-date>
-  <send-report-address>
-    <email-address>
-      <email>#{@options[:report_email]}</email>
-      <name>Send report for newsletter for location #{@options[:location_name]} sent out on #{@options[:send_out_date]}</name>
-    </email-address>
-  </send-report-address>
-  <status-report report-id="new" delete="false" user-tracking-details="false" link-tracking-details="false" bouncing-details="false">
-    <report-address>
+<control xmlns="http://webservices.ecircle-ag.com/ecm" request-id="1234" group-id="5678">
+  <message message-id="new" delete="false">
+    <sendout-preferences>
+      <object-handling html-images="untouched"/>
+      <email-channel preferred-format="email-html-multipart"/>
+    </sendout-preferences>
+    <send-date>
+      <date>#{Ecircle::Helper.date_format(@options[:send_out_date])}</date>
+    </send-date>
+    <send-report-address>
       <email-address>
         <email>#{@options[:report_email]}</email>
-        <name>Status report for newsletter for location #{@options[:location_name]} sent out on #{@options[:send_out_date]}</name>
+        <name>Send report for newsletter for location #{@options[:location_name]} sent out on #{@options[:send_out_date]}</name>
       </email-address>
-    </report-address>
-    <send-date>
-      <date>#{Ecircle::Helper.date_format(@options[:send_date_for_report])}</date>
-    </send-date>
-  </status-report>
-  <content target-content-encoding="ISO-8859-1">
-    <subject target-encoding="ISO-8859-1">#{@options[:subject]}</subject>
-    <text target-content-encoding="ISO-8859-1">#{@options[:text]}</text>
-    <html target-content-encoding="ISO-8859-1">#{@options[:html]}</html>
-  </content>
-</message>
-<success-report-address>
-  <email-address>
-    <email>#{@options[:report_email]}</email>
-    <name>Success report for newsletter for location #{@options[:location_name]} sent out on #{@options[:send_out_date]}</name>
-  </email-address>
-</success-report-address>
-<failure-report-address>
-  <email-address>
-    <email>#{@options[:report_email]}</email>
-    <name>Failure report for newsletter for location #{@options[:location_name]} sent out on #{@options[:send_out_date]}</name>
-  </email-address>
-</failure-report-address>
+    </send-report-address>
+    <status-report report-id="new" delete="false" user-tracking-details="false" link-tracking-details="false" bouncing-details="false">
+      <report-address>
+        <email-address>
+          <email>#{@options[:report_email]}</email>
+          <name>Status report for newsletter for location #{@options[:location_name]} sent out on #{@options[:send_out_date]}</name>
+        </email-address>
+      </report-address>
+      <send-date>
+        <date>#{Ecircle::Helper.date_format(@options[:send_date_for_report])}</date>
+      </send-date>
+    </status-report>
+    <content target-content-encoding="ISO-8859-1">
+      <subject target-encoding="ISO-8859-1">#{@options[:subject]}</subject>
+      <text target-content-encoding="ISO-8859-1">#{@options[:text]}</text>
+      <html target-content-encoding="ISO-8859-1">#{@options[:html]}</html>
+    </content>
+  </message>
+  <success-report-address>
+    <email-address>
+      <email>#{@options[:report_email]}</email>
+      <name>Success report for newsletter for location #{@options[:location_name]} sent out on #{@options[:send_out_date]}</name>
+    </email-address>
+  </success-report-address>
+  <failure-report-address>
+    <email-address>
+      <email>#{@options[:report_email]}</email>
+      <name>Failure report for newsletter for location #{@options[:location_name]} sent out on #{@options[:send_out_date]}</name>
+    </email-address>
+  </failure-report-address>
+</control>
 xml
   end
 
